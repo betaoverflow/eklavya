31cfdda3523094b0e3236eed51cad3cc4e7504f0
Aniket762 <aniketindian8@gmail.com>
2021-11-20 00:18:05 +0530

    Feat: Roadmap screen init

--- lib/screens/roadmap.dart
+++ lib/screens/roadmap.dart
@@ -10,9 +10,48 @@ class Roadmap extends StatefulWidget {
 class _RoadmapState extends State<Roadmap> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Center(
-      child: Text("Roadmap Screen"),
-    ));
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+          child: SingleChildScrollView(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              height: 250,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  image: DecorationImage(
+                    image: AssetImage('assets/uberLogo.jpg'),
+                    fit: BoxFit.cover,
+                  )),
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    gradient: LinearGradient(
+                        begin: Alignment.bottomRight,
+                        colors: [
+                          Colors.black.withOpacity(.4),
+                          Colors.black.withOpacity(.2)
+                        ])),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Jumpstart now",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      )),
+    );
   }
 }
