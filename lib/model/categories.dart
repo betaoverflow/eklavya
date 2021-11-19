77e28b3bc2fd94874e6e620ba34dd81a114135e3
Aniket762 <aniketindian8@gmail.com>
2021-11-19 11:34:08 +0530

    Feat:Category Model added

---
+++ lib/model/categories.dart
@@ -0,0 +1,12 @@
+class CategoryModel {
+  final name;
+  CategoryModel({this.name});
+}
+
+List<CategoryModel> categories = [
+  CategoryModel(name: "Internship"),
+  CategoryModel(name: "Full time"),
+  CategoryModel(name: "Opensource"),
+  CategoryModel(name: "Research"),
+  CategoryModel(name: "Others")
+];
