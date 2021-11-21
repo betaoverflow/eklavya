9e07540fdbc7b60830de18cd736a8bd852cfe818
Aniket762 <aniketindian8@gmail.com>
2021-11-22 01:18:52 +0530

    Feat: Mentor model added

---
+++ lib/model/mentor.dart
@@ -0,0 +1,9 @@
+class MentorModel {
+  final name;
+  final image;
+  final type;
+  final ratings;
+  final price;
+
+  MentorModel({this.name, this.type, this.ratings, this.price, this.image});
+}
