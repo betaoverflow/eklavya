25f09a7ea59b3e4b00218af5f4ae2cb305630cb9
Aniket762 <aniketindian8@gmail.com>
2021-11-22 18:39:35 +0530

    Feat:Onboarding Model added

---
+++ lib/model/onboarding.dart
@@ -0,0 +1,25 @@
+class OnboardingModel {
+  final image;
+  final title;
+  final descp;
+
+  OnboardingModel({this.image, this.title, this.descp});
+}
+
+List<OnboardingModel> onboardingContent = [
+  OnboardingModel(
+      title: "Title 1",
+      image:
+          "https://i.pinimg.com/originals/21/df/b9/21dfb950213ce7b9d879e81bed79fca6.png",
+      descp: "Describing the title jo hum likhenge turant"),
+  OnboardingModel(
+      title: "Title 1",
+      image:
+          "https://i.pinimg.com/originals/21/df/b9/21dfb950213ce7b9d879e81bed79fca6.png",
+      descp: "Describing the title jo hum likhenge turant"),
+  OnboardingModel(
+      title: "Title 1",
+      image:
+          "https://i.pinimg.com/originals/21/df/b9/21dfb950213ce7b9d879e81bed79fca6.png",
+      descp: "Describing the title jo hum likhenge turant")
+];
