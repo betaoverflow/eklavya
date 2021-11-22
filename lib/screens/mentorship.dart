5134d9650cf2bac3228aa2608f4cf1ec2aa4556c
Aniket762 <aniketindian8@gmail.com>
2021-11-22 18:22:34 +0530

    Feat: Mentorship Screen with Profile screen init

--- lib/screens/mentorship.dart
+++ lib/screens/mentorship.dart
@@ -1,18 +1,181 @@
 import 'package:flutter/material.dart';
+import 'package:eklavya/config/fonts.dart';
+import 'package:eklavya/model/mentor.dart';
+import 'package:eklavya/screens/profileScreen.dart';
+import 'package:eklavya/widget/ratings.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class Mentorship extends StatefulWidget {
-  const Mentorship({Key? key}) : super(key: key);
+class Mentorship extends StatelessWidget {
+  Mentorship({Key? key}) : super(key: key);
+  List<MentorModel> list = [
+    MentorModel(
+        name: "Sumana Basu",
+        image: "assets/sumana.png",
+        type: "Google",
+        price: 150,
+        ratings: 4),
+    MentorModel(
+        name: "Aniket Pal",
+        image: "assets/aniket.png",
+        type: "Directi",
+        price: 100,
+        ratings: 5),
+    MentorModel(
+        name: "Sumana Basu",
+        image: "assets/sumana.png",
+        type: "Facebook",
+        price: 170,
+        ratings: 3),
+    MentorModel(
+        name: "Aniket Pal",
+        image: "assets/aniket.png",
+        type: "Walmart",
+        price: 120,
+        ratings: 5),
+  ];
 
-  @override
-  _MentorshipState createState() => _MentorshipState();
-}
+  Widget _searchBar(context) {
+    return Container(
+      height: 60,
+      margin: EdgeInsets.symmetric(
+        vertical: 20,
+      ),
+      decoration: BoxDecoration(
+        color: Color(0xffd2d1e1).withOpacity(.3),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Row(
+        children: <Widget>[
+          SizedBox(width: 16),
+          Icon(Icons.search),
+          SizedBox(width: 12),
+          SizedBox(
+            width: MediaQuery.of(context).size.width - 150,
+            child: TextField(
+              decoration: InputDecoration(
+                  hintText: "Search",
+                  hintStyle: GoogleFonts.inter(fontSize: 17),
+                  border: InputBorder.none),
+            ),
+          ),
+          Spacer(),
+          Container(
+              width: 60,
+              height: 60,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: RotatedBox(
+                  quarterTurns: 1,
+                  child: Icon(Icons.filter_list, color: Colors.white)))
+        ],
+      ),
+    );
+  }
 
-class _MentorshipState extends State<Mentorship> {
+  Widget _cards(context, MentorModel model) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 12),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => ProfilePage(model: model)));
+        },
+        child: Row(
+          children: <Widget>[
+            Container(
+              height: 75,
+              width: 75,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  image: DecorationImage(
+                    image: AssetImage(model.image),
+                  ),
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                      blurRadius: 10,
+                      color: Colors.grey.shade400,
+                      offset: Offset(4, 4),
+                    )
+                  ]),
+            ),
+            SizedBox(
+              width: 12,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(model.name,
+                    style: GoogleFonts.inter(
+                        fontWeight: FontWeight.w600, fontSize: 15)),
+                SizedBox(height: 5),
+                Text(model.type,
+                    style: GoogleFonts.inter(
+                        fontWeight: FontWeight.w300,
+                        fontSize: 10,
+                        color: Colors.black54)),
+              ],
+            ),
+            Spacer(),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                // Ratings(ratings: model.ratings),
+                SizedBox(height: 5),
+                Text("\₹${model.price}/hr",
+                    style: GoogleFonts.inter(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 14,
+                        color: Colors.black)),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Center(
-      child: Text("Mentorship Screen"),
-    ));
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(height: 40),
+                    SizedBox(height: 30),
+                    Text("Find Your",
+                        style: GoogleFonts.inter(
+                            fontSize: 22,
+                            color: Colors.black87,
+                            fontWeight: FontWeight.w300)),
+                    Text("Perfect Mentor",
+                        style: GoogleFonts.inter(
+                            fontSize: 22,
+                            color: Colors.black87,
+                            fontWeight: FontWeight.w500)),
+                    SizedBox(height: 7),
+                    _searchBar(context),
+                    SizedBox(height: 25),
+                    SizedBox(height: 20),
+                    _cards(context, list[0]),
+                    _cards(context, list[1]),
+                    _cards(context, list[2]),
+                    _cards(context, list[3]),
+                  ],
+                ))
+          ],
+        )),
+      ),
+    );
   }
 }
---
+++ lib/screens/profileScreen.dart
@@ -0,0 +1,204 @@
+import 'package:flutter/material.dart';
+import 'package:eklavya/config/constants.dart';
+import 'package:eklavya/config/fonts.dart';
+import 'package:eklavya/model/mentor.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key? key, this.model}) : super(key: key);
+  final model;
+
+  Widget _cards(
+    context,
+  ) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 12),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => ProfilePage()));
+        },
+        child: Row(
+          children: <Widget>[
+            Container(
+              height: 85,
+              width: 85,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                  image: AssetImage(model.image),
+                ),
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                    blurRadius: 10,
+                    color: Colors.grey.shade400,
+                    offset: Offset(4, 4),
+                  ),
+                ],
+              ),
+            ),
+            Spacer(),
+            Container(
+                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 10),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey.withOpacity(.5)),
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Icon(Icons.star, color: Colors.yellow, size: 15),
+                        SizedBox(height: 5),
+                        Text("${model.ratings}",
+                            style: GoogleFonts.inter(
+                                fontWeight: FontWeight.w300, fontSize: 10)),
+                      ],
+                    ),
+                    SizedBox(width: 12),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Text("Reviews", style: GoogleFonts.inter(fontSize: 8)),
+                        SizedBox(height: 8),
+                        Text("1500",
+                            style: GoogleFonts.inter(
+                                fontWeight: FontWeight.w300,
+                                fontSize: 10,
+                                color: Colors.black54)),
+                      ],
+                    ),
+                  ],
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _description() {
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(model.name,
+                      style: GoogleFonts.inter(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 15,
+                          height: 1.3)),
+                  SizedBox(height: 5),
+                  Text(model.type,
+                      style: GoogleFonts.inter(
+                          fontWeight: FontWeight.w300,
+                          fontSize: 10,
+                          color: Colors.black54)),
+                ],
+              ),
+              Spacer(),
+              Text("\$${model.price}/hr",
+                  style: GoogleFonts.inter(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 14,
+                      color: Colors.black)),
+            ],
+          ),
+          SizedBox(height: 20),
+          Text("About ${model.name.split(" ")[0]}",
+              style: GoogleFonts.inter(
+                  fontWeight: FontWeight.w600, fontSize: 15, height: 1.3)),
+          SizedBox(height: 5),
+          Text("${Constants.description.substring(0, 350)}",
+              style: GoogleFonts.inter(
+                  fontWeight: FontWeight.w300,
+                  fontSize: 12,
+                  height: 1.4,
+                  color: Colors.black54)),
+        ]);
+  }
+
+  Widget _achivment() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text("Achievments",
+            style: GoogleFonts.inter(
+                fontWeight: FontWeight.w600, fontSize: 15, height: 1.3)),
+        SizedBox(height: 16),
+        _achivmentCard(),
+        SizedBox(height: 16),
+        _achivmentCard(),
+        SizedBox(height: 16),
+      ],
+    );
+  }
+
+  Widget _achivmentCard() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.grey.withOpacity(.1),
+      ),
+      child: ListTile(
+        contentPadding: EdgeInsets.zero,
+        leading: Container(
+            width: 40, alignment: Alignment.center, child: Icon(Icons.people)),
+        title: Text("Mentored 100+ Students",
+            style:
+                GoogleFonts.inter(fontWeight: FontWeight.w600, fontSize: 15)),
+        subtitle: Text(Constants.description.substring(0, 90),
+            style:
+                GoogleFonts.inter(fontWeight: FontWeight.w400, fontSize: 12)),
+      ),
+    );
+  }
+
+  Widget _button() {
+    return FlatButton(
+      color: Colors.black,
+      onPressed: () {},
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      child: Container(
+        height: 60,
+        alignment: Alignment.center,
+        child: Text("Hire Now",
+            style: GoogleFonts.inter(
+              color: Colors.white,
+              fontWeight: FontWeight.w500,
+            )),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 18),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: 20),
+                _cards(context),
+                SizedBox(height: 20),
+                _description(),
+                SizedBox(height: 40),
+                _achivment(),
+                _button(),
+                SizedBox(height: 20),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
