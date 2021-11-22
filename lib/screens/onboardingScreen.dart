d9f13980c245e8a49f12132161d4111da320038a
Aniket762 <aniketindian8@gmail.com>
2021-11-22 18:52:43 +0530

    Feat: onboarding screen added

--- lib/screens/onboardingScreen.dart
+++ lib/screens/onboardingScreen.dart
@@ -1,3 +1,5 @@
+import 'package:eklavya/home.dart';
+import 'package:eklavya/model/onboarding.dart';
 import 'package:flutter/material.dart';
 
 class Onboarding extends StatefulWidget {
@@ -8,10 +10,117 @@ class Onboarding extends StatefulWidget {
 }
 
 class _OnboardingState extends State<Onboarding> {
+  int currentIndex = 0;
+  late PageController _controller;
+
+  @override
+  void initState() {
+    _controller = PageController(initialPage: 0);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[100],
+        body: Column(
+          children: [
+            Expanded(
+              child: PageView.builder(
+                controller: _controller,
+                itemCount: onboardingContent.length,
+                onPageChanged: (int index) {
+                  setState(() {
+                    currentIndex = index;
+                  });
+                },
+                itemBuilder: (_, i) {
+                  return Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      children: [
+                        SizedBox(height: 130),
+                        Image.network(
+                          onboardingContent[i].image,
+                          height: 250,
+                          width: 300,
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          onboardingContent[i].title,
+                          style: TextStyle(
+                              fontSize: 35, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          onboardingContent[i].descp,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 18, color: Colors.grey),
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(
+                    onboardingContent.length, (index) => dots(index, context)),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(50),
+              width: double.infinity,
+              height: (50),
+              child: ElevatedButton(
+                onPressed: () {
+                  if (currentIndex == onboardingContent.length - 1) {
+                    Navigator.push(
+                        // context, MaterialPageRoute(builder: (_) => LoginPage()));
+                        context,
+                        MaterialPageRoute(builder: (_) => Home()));
+                  }
+                  _controller.nextPage(
+                      duration: Duration(milliseconds: 100),
+                      curve: Curves.bounceIn);
+                },
+                style: (ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.black),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    )))),
+                child: Text(
+                  currentIndex == onboardingContent.length - 1
+                      ? 'Done'
+                      : 'Next',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16),
+                ),
+              ),
+            ),
+          ],
+        ));
+  }
+
+  Container dots(int index, BuildContext context) {
     return Container(
-      child: Text("Onboarding Screen"),
+      height: 10,
+      width: currentIndex == index ? 20 : 10,
+      margin: EdgeInsets.only(right: 5),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: currentIndex == index ? Colors.black : Colors.grey),
     );
   }
 }
