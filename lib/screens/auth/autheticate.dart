3aea703700962dde71d8e08f829999ca97793094
Aniket762 <aniketindian8@gmail.com>
2021-11-24 14:28:36 +0530

    Feat: authentication logic added

---
+++ lib/screens/auth/autheticate.dart
@@ -0,0 +1,17 @@
+import 'package:eklavya/screens/auth/loginScreen.dart';
+import 'package:eklavya/screens/mentorship.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class Authenticate extends StatelessWidget {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    if (_auth.currentUser != null) {
+      return Mentorship();
+    } else {
+      return LoginScreen();
+    }
+  }
+}
